fb46e5434422f6bc0f81e97eb8a8fb70c3eafbd1
Sebastian <71264234+sebastianbartolo@users.noreply.github.com>
2020-09-16 21:32:41 +1000

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Sebastian Kieth Bartolo on 16/9/20.
+//  Copyright © 2020 Sebastian Kieth Bartolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
