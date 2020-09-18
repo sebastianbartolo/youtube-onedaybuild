77e3e87e601ea7094e4ad512fe410ed0fe0e1f17
sebastianbartolo <71264234+sebastianbartolo@users.noreply.github.com>
2020-09-18 22:14:01 +1000

    Update ConstantsTemplate.swift

--- youtube-onedaybuild/ConstantsTemplate.swift
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -10,10 +10,9 @@ import Foundation
 
 //put your own details down below
 
-/*
+
 struct Constants {
-    static var API_KEY = ""
-    static var PLAYLIST_ID = ""
+    static var API_KEY = "AIzaSyCvG0BGAgQTskB8AMRXqGA8d2Mymt9QSF0"
+    static var PLAYLIST_ID = "PLi-xKOcBJmgjMQLRqhN9f2Eco4o8r1ejS"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
-*/
